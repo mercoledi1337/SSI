685ab995c331d7b7626278b6e20492a09365b380
Michał Sereda <mercoledics@gmail.com>
2025-11-16 16:21:10 +0100

    ploting my own csv

--- main.cpp
+++ main.cpp
@@ -12,35 +12,34 @@ int main()
 {
     Gnuplot gp("D:/gnuplot/bin/gnuplot.exe");
 
-    srand (time(NULL));
-    ifstream iFile;
+    //reading csv part
+    ifstream iFile("../spiralka.csv");
+    if (!iFile.is_open()) {
+        cerr << "Nie można otworzyć pliku spiralka.csv" << endl;
+        return 1;
+    }
+    string line = "", first = "", second = "";
+    int location = 0, readcount = 0;
+    vector<vector<float>> spiralka;
+    const int MAX_SIZE = 101;
 
-    random_device rd;
-    mt19937 mt(rd());
-    normal_distribution<double> distribution(0., 1.);
+    while (getline(iFile, line) && readcount < MAX_SIZE)
+    {
+        float group = readcount%4;
+        location = line.find(',');
+        first = line.substr(0, location);
+        line = line.substr(location + 1, line.length());
+        spiralka.push_back({stof(first), stof(line),group});
 
-    vector<double> v0, v1;
-    for (int i = 0; i < 1000; i++) {
-        v0.push_back(distribution(mt));
-        v1.push_back(distribution(mt));
+        readcount++;
     }
 
-    // Liczenie sumy częściowej
-    partial_sum(v0.begin(), v0.end(), v0.begin());
-    partial_sum(v1.begin(), v1.end(), v1.begin());
-
-    // Przygotowanie danych w formacie (x, y) dla gnuplot
-    vector<pair<int,double>> data_v0, data_v1;
-    for (size_t i = 0; i < v0.size(); ++i) {
-        data_v0.push_back({static_cast<int>(i), v0[i]});
-        data_v1.push_back({static_cast<int>(i), v1[i]});
-    }
-
+    string kolor = "green";
     // Wysyłanie do gnuplota
-    gp << "set title 'Wykres v0 i v1'\n";
-    gp << "plot '-' with lines title 'v0', '-' with lines title 'v1'\n";
-    gp.send1d(data_v0);  // pierwsza seria
-    gp.send1d(data_v1);  // druga seria
+    gp << "set title 'Wykres spiralka'\n";
+    gp << "plot '-' with points pt 7 ps 1.5 lc rgb '" + kolor + "' title 'dane'\n";
+    gp.send1d(spiralka);  // pierwsza seria
+
 
     cout << "Wykres wysłany do gnuplota. Naciśnij Enter, aby zakończyć..." << endl;
     cin.get();
@@ -48,25 +47,11 @@ int main()
     const int M = 4;
     const int iters = 100;
     float V[4]; // mean
-    const int MAX_SIZE = 101;
-    string line = "", first = "", second = "";
-    int location = 0, readcount = 0;
 
-    iFile.open("spiralka.csv");
 
-    float spiralka[MAX_SIZE][3];
 
-    while (getline(iFile, line) && readcount < MAX_SIZE)
-    {
-        location = line.find(',');
-        first = line.substr(0, location);
-        line = line.substr(location + 1, line.length());
-        spiralka[readcount][0] = stof(first);
-        spiralka[readcount][1] = stof(line);
-        spiralka[readcount][2] = 9;
 
-        readcount++;
-    }
+
     // choosing center of groups
     for (int i = 0; i < M; i++) {
         V[i] = rand() % 102;
