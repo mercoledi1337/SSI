2a54bad7177837f31fe38de1dbff57d51bf2ff10
Michał Sereda <mercoledics@gmail.com>
2025-11-22 18:28:57 +0100

    ploing iterations and extracts some code to fucntions

--- main.cpp
+++ main.cpp
@@ -42,17 +42,58 @@ void euclidesForKMean(vector<vector<float>>& spiralka_po, vector<vector<float>> 
             if (distance <= tmpShortest)
             {
                 tmpShortest = distance;
-                spiralka_po[s][2] = groups[i][2];
+                spiralka_po[s][2] = i;
             }
         }
     }
 }
 
+void countGroups(vector<vector<float>>& spiralka_po, vector<int>& groupSize)
+{
+    for (int i = 0; i < M; i++)
+    {
+        int var = 0;
 
+        for (int j = 0; j < 101; ++j) {
+            var += count(spiralka_po[j].begin(), spiralka_po[j].end(), i);
+        }
+
+        if (groupSize.size() < M) {
+            groupSize.push_back(var);
+        } else {
+            groupSize[i] = var;
+        }
+    }
+}
+
+void calcMean(vector<vector<float>> spiralka_po, vector<vector<float>>& groups, vector<int>& groupSize) {
+    for (int i = 0; i < M; i++) { // looping by groups
+        float meanX = 0;
+        float meanY = 0;
+        if (groupSize[i] == 0)
+            continue;
+        for (int j = 0; j < iters; j++) { // looping whole vector
+            if (spiralka_po[j][2] == i) { //if group in spiralka_po is valid add it to meanX/meanY
+                meanX += spiralka_po[j][0];
+                meanY += spiralka_po[j][1];
+            }
+        }
+        meanX /= groupSize[i];
+        meanY /= groupSize[i];
+        float g = i;
+        if (groups.size() < M)
+            groups.push_back({meanX, meanY, g});
+        else {
+            groups[i][0] = meanX;
+            groups[i][1] = meanY;
+            groups[i][2] = g;
+        }
+    }
+}
 int main()
 {
     float V[4]; // mean
-    int groupSize[4];
+    vector<int> groupSize;
 
     vector<vector<float>> spiralka;
     vector<vector<float>> groups;
@@ -93,54 +134,42 @@ int main()
     }
 
     auto spiralka_po = spiralka; // kopia danych
-    gp << "set multiplot layout 1,2 title 'K-Means'\n";
-    // Wysyłanie do gnuplota
+
+
+    // Wysyłanie startowego do gnuplota
+    gp << "set term wxt 1\n";
     gp << "set title 'Wykres spiralka'\n";
     gp << "plot '-' using 1:2:3 with points pt 7 lc palette notitle\n";
     gp.send1d(spiralka);  // pierwsza seria
-
-    //find size of each group then loop inside
-    for (int i = 0; i < M; i++)
-    {
-        int var = 0;
-        for (auto& v: spiralka_po) {
-             var += count(v.begin(), v.end(), i);
-        }
-        groupSize[i] = var;
-    }
-
-    //looping in groups
-    for (int i = 0; i < M; i++) { // looping by groups
-        float meanX = 0;
-        float meanY = 0;
-        if (groupSize[i] == 0)
-            continue;
-        for (int j = 0; j < iters; j++) { // looping whole vector
-            if (spiralka_po[j][2] == i) { //if group in spiralka_po is valid add it to meanX/meanY
-                meanX += spiralka_po[j][0];
-                meanY += spiralka_po[j][1];
-            }
-        }
-        meanX /= groupSize[i];
-        meanY /= groupSize[i];
-        float g = i;
-        groups.push_back({meanX, meanY, g});
-    }
-
-    euclidesForKMean(spiralka_po, groups, iters, M);
-
-    //coloring centers;
-    for (int i = 0; i < M; i++) {
-        groups[i][2] = 5;
-        spiralka_po.push_back(groups[i]);
-    }
-
-    // Wysyłanie do gnuplota
-    gp << "set title 'Wykres spiralka Po'\n";
-    gp << "plot '-' using 1:2:3 with points pt 7 lc palette notitle\n";
-    gp.send1d(spiralka_po);  // druga seria
-    cout << "Wykres wysłany do gnuplota. Naciśnij Enter, aby zakończyć..." << endl;
+    cout << "Wykres wyslany do gnuplota. Nacisnij Enter, aby zakonczyc..." << endl;
     cin.get();
 
+
+    // Wysyłanie po iteracji do gnuplota
+    for (int j = 2; j < 5; j++) {
+        countGroups(spiralka_po, groupSize);
+        calcMean(spiralka_po,groups,groupSize);
+        euclidesForKMean(spiralka_po, groups, iters, M);
+
+        //coloring centers;
+        if (spiralka_po.size() == 101) {
+            for (int i = 0; i < M; i++) {
+                groups[i][2] = 5;
+                spiralka_po.push_back(groups[i]);
+            }
+        } else {
+            for (int i = 0; i < M; i++) {
+                groups[i][2] = 5;
+                spiralka_po[101+i][2] = 5;
+            }
+        }
+
+        gp << "set term wxt "  << j  << "\n";
+        gp << "set title 'Wykres spiralka'\n";
+        gp << "plot '-' using 1:2:3 with points pt 7 lc palette notitle\n";
+        gp.send1d(spiralka_po);  // pierwsza seria
+        cout << "Wykres wyslany do gnuplota. Nacisnij Enter, aby zakonczyc..." << endl;
+        cin.get();
+    }
     return 0;
 }
